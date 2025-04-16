316f50e6bcdedd789a77e343b0fcf30f482db3c7
Sergei Korolenko <000korsa000@gmail.com>
2025-04-17 00:53:01 +0300

    Добавил PriceProvider

---
+++ Assets/Scripts/Core/UI/PriceProvider.cs
@@ -0,0 +1,32 @@
+using Core.Candles;
+using UnityEngine;
+using Zenject;
+using TMPro;
+
+namespace Core.UI
+{
+    public class PriceProvider : MonoBehaviour
+    {
+        [SerializeField] 
+        private TextMeshProUGUI priceText;
+
+        [Inject] private readonly CoreEventBus _coreEventBus;
+        
+        private void Awake()
+        {
+            _coreEventBus.OnCurrentPriceUpdated += UpdatePriceText;
+        }
+
+        private void OnDestroy()
+        {
+            _coreEventBus.OnCurrentPriceUpdated -= UpdatePriceText;
+        }
+
+        private void UpdatePriceText(CandlePresenter currentCandle)
+        {
+            float price = currentCandle.CurrentPrice;
+            string newText = $"{price:F2} $";
+            priceText.SetText(newText);
+        }
+    }
+}
